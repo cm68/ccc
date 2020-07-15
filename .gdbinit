d181dd630643abcf7762f9c57361511023e4dd43
curt Mayer <curt@zen-room.org>
2020-07-15 00:28:58 -0700

    gdb

---
+++ .gdbinit
@@ -0,0 +1,5 @@
+set print pretty
+define hook-quit
+	set confirm off
+end
+source .gdbargs
---
+++ tests/decl.c
@@ -0,0 +1,11 @@
+int i;
+int *ip;
+
+char c;
+char *cp;
+
+unsigned char uc;
+unsigned char *ucp;
+
+int ia[20];
+int *ipa[20];
