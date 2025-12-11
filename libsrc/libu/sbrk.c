a7ee9540275dde5a7a984123722cdb2d9a4b529c
curt mayer <curt@zen-room.org>
2025-12-11 02:33:04 -0800

    more syscall interface hackery

---
+++ libsrc/libu/sbrk.c
@@ -0,0 +1,32 @@
+/*
+ * c wrapper around _break system call 
+ */
+
+unsigned short memtop;	/* no idea who initializes this */
+
+int
+brk(unsigned short addr)
+{
+	unsigned short ret;
+    unsigned char array[4];
+
+	if (addr < memtop && addr > array) {
+		return -1;
+	}
+	ret = _break(addr);
+	if (ret == -1) {
+		return -1;
+	}
+	memtop = ret;
+	return memtop;
+}
+
+int
+sbrk(unsigned short increment)
+{
+	return (brk(memtop+increment));
+}
+
+/*
+ * vim: tabstop=4 shiftwidth=4 expandtab:
+ */
