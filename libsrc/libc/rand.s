86afd68b1f9ed5721c68edc5a4da7c9974d2f394
curt mayer <curt@zen-room.org>
2026-01-08 16:44:04 -0800

    libc changes

--- libsrc/libc/rand.s
+++ libsrc/libc/rand.s
@@ -7,20 +7,20 @@ _srand:
 	push	iy
 	ld	iy,0
 	add	iy,sp
-; x=4
 	jp	L1
-L2:ld	hl,(iy+4)
+L2:	ld	hl,(iy+4)
 	ld	(_randx),hl
-ld	a,h
-rla
-sbc	a,a
-ld	h,a
-ld	l,a
-ld	(_randx+22),hl
+	ld	a,h
+	rla
+	sbc	a,a
+	ld	h,a
+	ld	l,a
+	ld	(_randx+22),hl
 L3:	ld	sp,iy
 	pop	iy
 	ret
 L1:	jp	L2
+
 .globl	_rand
 .text
 _rand:
@@ -28,28 +28,16 @@ _rand:
 	ld	iy,0
 	add	iy,sp
 	jp	L4
-L5:ld	hl,(471552)
-push	hl
-
-push	hl
-ld	hl,(_randx+22)
-push	hl
-	ld	hl,(_randx)
-push	hl
-call	call lmul
-	ld	(_randx),hl
-	ld	hl,(_randx)+2
-ld	(hl),e
-inc	hl
-ld	(hl),d
-ld	de,-20
-call lshl
-ld	de,-100000
-call andn16
+L5:	ld	bc,77777
+	add	hl,bc
+	ex	de,hl
+	ld	hl,0
+	ex	de,hl
 	ex de,hl
 	jp	L6
 L6:	ld	sp,iy
 	pop	iy
 	ret
 L4:	jp	L5
+
 .data
